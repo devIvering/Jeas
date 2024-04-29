ecef7feb6519f455e018db5ff512b38fb42a12d7
Gustav Ivering <gustav.ivering@live.se>
2024-04-29 20:56:52 +0200

    updated website

---
+++ Jeas-Trycksaker/Pages/Categories/Season.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Jeas_Trycksaker.Pages.Categories
+{
+    public class SeasonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
